af0b00dfe759444b28efeb6e61ebc2dbe3ea2045
Christopher Diggins <cdiggins@gmail.com>
2023-03-27 02:12:40 -0400

    Checking immutability

--- Platonic/Platonic/Program.cs
+++ Platonic/Platonic/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using PlatoAnalyzer;
 using Ptarmigan.Utils.Roslyn;
 using Compilation = Ptarmigan.Utils.Roslyn.Compilation;
@@ -9,13 +11,40 @@ namespace Platonic
 {
     public static class Program
     {
+        //public static bool ReferencesAMutableO  
+
+        public static bool IsImmutable(ITypeSymbol symbol, List<string> reasons)
+        {
+            // All fields are readonly 
+            foreach (var m in symbol.GetDeclaredAndBaseMembers())
+            {
+                if (m is IFieldSymbol fs)
+                {
+                    if (!fs.IsReadOnly && !fs.IsConst)
+                    {
+                        //if (fs.IsPublicMember())
+                        reasons.Add($"Type has a public field {fs.Name} that is not readonly or const");
+                    }
+                }
+            }
+                
+            return reasons.Count == 0;
+        }
+
+        public static void AnalyzeClass(TypeDeclarationSyntax typeDecl, INamedTypeSymbol symbol)
+        {
+            // Is this a mutable class or not? 
+
+            // If it is mutable, is it used in a non-mutable context 
+        }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Starting compilation ... ");
             var inputFolder = @"C:\Users\cdigg\git\plato\Platonic\TestInput";
             var inputFiles = Directory.GetFiles(inputFolder, "*.cs");
-
             var compilation = Compilation.Create(inputFiles);
+            Console.WriteLine($"Compilation completed {compilation.EmitResult}");
 
             //PlatoToCSharp.OutputTypes(compilation.Compiler, outputFile);
             /*
@@ -42,17 +71,15 @@ namespace Platonic
 
             var d = new Dictionary<ISymbol, int>();
             var d2 = new Dictionary<ISymbol, int>();
-
             foreach (var (td, sym) in compilation.GetTypeDeclarationsWithSymbols())
             {
-                Console.WriteLine($"{td.Keyword} {sym.Name} {sym.Kind}");
+                // Console.WriteLine($"{td.Keyword} {sym.Name} {sym.Kind}");
                 d[sym] = 0;
             }
 
             var namedTypeSymbols = compilation
-                .GetAllSymbols()
-                .OfType<INamedTypeSymbol>();
-            
+                .GetExpressionAndTypeSymbols()
+                .OfType<INamedTypeSymbol>();            
 
             var distinctNamedTypeSymbols = namedTypeSymbols
                 .Distinct();
@@ -72,17 +99,30 @@ namespace Platonic
                 }
             }
 
+            
             Console.WriteLine("Declared symbols");
             foreach (var kv in d)
             {
-                Console.WriteLine($"{kv.Key} = {kv.Value}");
+                //Console.WriteLine($"{kv.Key} = {kv.Value}");
+
+                if (kv.Key is ITypeSymbol ts)
+                {
+                    var reasons = new List<string>();
+                    var mut = IsImmutable(ts, reasons);
+                    Console.WriteLine($"{ts.Name} is immutable {mut}");
+                    foreach (var reason in reasons)
+                    {
+                        Console.WriteLine($"  {reason}");
+                    }
+                }
             }
 
+            /*
             Console.WriteLine("Undeclared symbols");
             foreach (var kv in d2)
             {
                 Console.WriteLine($"{kv.Key} = {kv.Value}");
-            }
+            }*/
 
             // TODO: get references to members. 
             // TODO: properly handle generics 
