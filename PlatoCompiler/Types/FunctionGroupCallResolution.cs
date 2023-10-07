b748628a438c96beb756835f9d49e0a55ec28ebe
Christopher Diggins <cdiggins@gmail.com>
2023-10-06 23:36:04 -0400

    Working on better resolution of function overloads

--- PlatoCompiler/Types/FunctionGroupCallResolution.cs
+++ PlatoCompiler/Types/FunctionGroupCallResolution.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Plato.Compiler.Symbols;
 
@@ -29,6 +30,75 @@ namespace Plato.Compiler.Types
                 .ToList();
         }
 
+        public bool IsConcept(IType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConcreteType(IType type) { throw new NotImplementedException(); }
+
+        public int InheritanceDepth(IType type, IType typeBase)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ImplementsDepth(IType type, IType concept)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 0 is a perfect fit, below 0 is a non-fit, above 0 is an increasingly bad fit. 
+        /// </summary>
+        public int ArgumentFit(IType typeArgument, IType typeParameter)
+        {
+            if (typeArgument.Equals(typeParameter))
+                return 0;
+
+            if (IsConcept(typeArgument))
+            {
+                if (IsConcept(typeParameter))
+                {
+                    return InheritanceDepth(typeArgument, typeParameter);
+                }
+
+                if (IsConcreteType(typeParameter))
+                {
+                    // Cannot pass a concept to a concrete type 
+                    return -1;
+                }
+
+                // TODO: could be a type-variable 
+
+                throw new Exception("Neither concrete not interface");
+            }
+
+            if (IsConcreteType(typeArgument))
+            {
+                if (IsConcept(typeParameter))
+                {
+                    return ImplementsDepth(typeArgument, typeParameter);
+                }
+
+                if (IsConcreteType(typeParameter))
+                {
+                    // Concrete types are different 
+                    // TODO: log this
+                    return -1;
+                }
+
+                // TODO: could be a type-variable 
+
+                throw new Exception("Neither concrete not interface");
+            }
+
+            // TODO: could be a type 
+
+            // Really don't know 
+            return 9999;
+        }
+
+        
         public bool CanCall(FunctionAnalysis fa, IReadOnlyList<IType> argTypes)
         {
             if (fa.Parameters.Children.Count != argTypes.Count)
